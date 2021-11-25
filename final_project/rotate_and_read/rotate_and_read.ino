7bcf2e7003870c2ba636614d936ce9497926b492
James Ward <vz21101@bristol.ac.uk>
2021-11-25 16:28:00 +0000

    Code for rotating and reading

--- final_project/rotate_and_read/rotate_and_read.ino
+++ final_project/rotate_and_read/rotate_and_read.ino
@@ -10,6 +10,10 @@ u8 USB_SendSpace(u8 ep);
 #define PID_UPDATE 10
 
 Kinematics_c robot_kinematics;
+irSensors_c ir_sensors;
+Motor_c motors;
+PID_c left_controller;
+PID_c right_controller;
 
 long last_e_left;
 long last_e_right;
@@ -24,12 +28,17 @@ unsigned long pid_ts;
 unsigned long elapsed_t;
 
 float heading_demand = 2*3.1415926;
-float drive_demand = 30;
+float drive_demand = 20;
+
+unsigned long intensity_readings[36];
+unsigned long elapsed_times[2];
+
 
 void setup() {
-  //left_controller.initialise(0.02, 0.8, 0.05);
-  //right_controller.initialise(0.02, 0.8, 0.05);
-  //motors.initialise();
+  left_controller.initialise(0.02, 0.8, 0.05);
+  right_controller.initialise(0.02, 0.8, 0.05);
+  motors.initialise();
+  ir_sensors.initialise();
   
   setupEncoderRight();
   setupEncoderLeft();
@@ -45,59 +54,67 @@ void setup() {
 
 void loop() {
 
-  //unsigned long current_ts;
-  //current_ts = millis();
+  unsigned long current_ts;
+  current_ts = millis();
 
-  //long pid_check = current_ts - pid_ts;
-  
+  long pid_check = current_ts - pid_ts;
+
   update_kinematics();
-  Serial.println(robot_kinematics.theta, 3);
-  delay(100);
-  //float current_theta = robot_kinematics.theta; 
 
-  //if(abs(heading_demand - current_theta) > 0.01) {
+  float current_theta = robot_kinematics.theta;
 
-    //if(pid_check > PID_UPDATE) {
+  if(abs(heading_demand - current_theta) > 0.05) {
 
-      //left_drive = left_controller.update(-1*drive_demand, last_v_left, pid_check);
-      //right_drive = right_controller.update(drive_demand, last_v_right, pid_check);
+    if(pid_check > PID_UPDATE) {
   
-      //last_v_left = left_drive;
-      //last_v_right = right_drive;
+      left_drive = left_controller.update(-1*drive_demand, last_v_left, pid_check);
+      right_drive = right_controller.update(drive_demand, last_v_right, pid_check);
     
-      //motors.setMotorPower("left", left_drive);
-      //motors.setMotorPower("right", right_drive);
+      last_v_left = left_drive;
+      last_v_right = right_drive;
+      
+      motors.setMotorPower("left", left_drive);
+      motors.setMotorPower("right", right_drive);
+    
+      pid_ts = millis();
   
-      //pid_ts = millis();
+      int current_theta_int = int(current_theta*5.7296);
+      Serial.println(current_theta_int);
+      
+      //if(current_theta_int % 10 == 0 || current_theta_int % 10 == 1) {
 
-      //if(current_theta == 10) {
-        
-        //ir_sensors.readAllSensors(elapsed_times)
+      if (not intensity_readings[current_theta_int]) {
+          
+        Serial.println("----");
+          
+        ir_sensors.readAllSensors(elapsed_times);
+        intensity_readings[current_theta_int] = elapsed_times[0] + elapsed_times[1];
+      }
       //}
+    }
+  }
+
+  else {
+
+    delay(25);
+
+    left_drive = 0;
+    right_drive = 0;
+
+    motors.setMotorPower("left", left_drive);
+    motors.setMotorPower("right", right_drive);
     
-    //}
-
-  //}
-
-  //else {
-
-    //delay(25);
-
-    //left_drive = 0;
-    //right_drive = 0;
-
-    //motors.setMotorPower("left", left_drive);
-    //motors.setMotorPower("right", right_drive);
+    update_kinematics();
     
-    //update_kinematics();
-    
-    //while(true) {
+    while(true) {
       
-      //reportResultsOverSerial();
-      //delay(1000);
+      reportResultsOverSerial();
+      delay(1000);
       
-    //}
-  //}
+    }
+  }
+
+    delay(1);
 
 }
 
@@ -127,11 +144,11 @@ void reportResultsOverSerial() {
   // results collected
 
   if( SERIAL_ACTIVE ) {
-    Serial.print(robot_kinematics.x);
-    Serial.print(" ,");
-    Serial.print(robot_kinematics.y);
-    Serial.print(" ,");
-    Serial.println(robot_kinematics.theta - heading_demand, 3);
+    for(int i = 0; i < 36; i++) {
+      Serial.print(intensity_readings[i]);
+      Serial.print(", ");
+    }
+    Serial.println("");
   }
 
   if( SERIAL_ACTIVE ) Serial.println( "---End of Results ---\n\n" ); 
